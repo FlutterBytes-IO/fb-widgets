d117d32aab729354a947b9a470c9ad25d98591ff
Yczar <babalolagbogo@gmail.com>
2022-10-21 01:43:11 +0100

    updated: the main.dart file

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:fb_widgets/fb_widgets.dart';
 
 void main() {
@@ -16,46 +14,79 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  final _fbWidgetsPlugin = FbWidgets();
+  final String _platformVersion = 'Unknown';
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await _fbWidgetsPlugin.getPlatformVersion() ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorObservers: [
+        FBWidgets.instance.navigatorObserver,
+      ],
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Column(
+          children: [
+            Center(
+              child: Text('Running on: $_platformVersion\n'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                FBWidgets.showSnackBar('Hello World');
+              },
+              child: const Text(
+                'Show Snackbar',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                FBWidgets.showModalBottomSheet();
+              },
+              child: const Text(
+                'Show Modal Bottom Sheet',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                FBWidgets.showBottomSheet();
+              },
+              child: const Text(
+                'Show Bottom Sheet',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                FBWidgets.showDialog();
+              },
+              child: const Text(
+                'Show Dialog',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                FBWidgets.showNotification();
+              },
+              child: const Text(
+                'Show Notification',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                FBWidgets.flashMessage(
+                  'Flash Message',
+                );
+              },
+              child: const Text(
+                'Show Flash Message',
+              ),
+            ),
+          ],
         ),
       ),
     );
