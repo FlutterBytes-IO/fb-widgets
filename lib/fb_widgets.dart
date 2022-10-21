9205f3c05403f4981ac4a4ad7ea04eb60c05a115
Yczar <babalolagbogo@gmail.com>
2022-10-21 01:42:40 +0100

    created: the FBWidgets class

--- lib/fb_widgets.dart
+++ lib/fb_widgets.dart
@@ -1,8 +1,60 @@
+import 'package:fb_widgets/utils.dart';
+import 'package:flutter/material.dart';
 
-import 'fb_widgets_platform_interface.dart';
+class FBWidgets {
+  FBWidgets._();
 
-class FbWidgets {
-  Future<String?> getPlatformVersion() {
-    return FbWidgetsPlatform.instance.getPlatformVersion();
+  ///
+  static final FBWidgets _instance = FBWidgets._();
+  static FBWidgets get instance => _instance;
+
+  ///
+  NavigatorObserver get navigatorObserver => Utils.instance.navigatorObserver;
+
+  ///
+  FBWidgets.showSnackBar(String message) {
+    final snackBar = SnackBar(
+      content: Text(message),
+      duration: const Duration(seconds: 3),
+    );
+    ScaffoldMessenger.of(Utils.instance.fbContext).showSnackBar(snackBar);
+  }
+
+  ///
+  FBWidgets.flashMessage(String message) {
+    final snackBar = SnackBar(
+      content: Text(message),
+      duration: const Duration(seconds: 3),
+    );
+    ScaffoldMessenger.of(Utils.instance.fbContext).showSnackBar(snackBar);
+  }
+
+  ///
+  FBWidgets.showDialog() {
+    showDialog(
+      context: Utils.instance.fbContext,
+      builder: (context) => Container(),
+    );
+  }
+
+  ///
+  FBWidgets.showModalBottomSheet() {
+    showModalBottomSheet(
+      context: Utils.instance.fbContext,
+      builder: (context) => Container(),
+    );
+  }
+
+  ///
+  FBWidgets.showBottomSheet() {
+    showBottomSheet(
+      context: Utils.instance.fbContext,
+      builder: (context) => Container(),
+    );
+  }
+
+  ///
+  FBWidgets.showNotification() {
+    // FBNotification.instance.showNotification();
   }
 }
